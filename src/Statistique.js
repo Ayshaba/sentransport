65840cf73bd8c8dc296eb73b6a913bf105c8538b
Aysha <aissataba395@gmail.com>
2026-05-02 18:04:49 +0000

    Lab 2 : composants statistiques LigneBus et ListeLignes

---
+++ src/Statistique.js
@@ -0,0 +1,12 @@
+import './Statistique.css';
+
+function Statistique() {
+    return (
+        <div className="statistique">
+            <h2>10</h2>
+            <p>Lignes</p>
+        </div>
+    );
+}
+
+export default Statistique;
